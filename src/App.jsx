8b4d73f1ec253898ae8265208d0ae589ccaff267
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-18 09:07:30 +0330

    Added Folder Structures

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+      <h1 className="text-white text-4xl font-bold shadow-lg">Contact App</h1>
+    </div>
+  );
+};
+
+export default App;
