7a6f7bd9cda1e186bbb24f85bd9b6c466112afdd
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-18 09:36:24 +0330

    Added Contact Module

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Contacts from "./components/modules/Contacts";
+
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
       <h1 className="text-white text-4xl font-bold shadow-lg">Contact App</h1>
+      <Contacts />
     </div>
   );
 };
---
+++ src/components/modules/Contacts.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const Contacts = () => {
+  // ============ State ============
+  const [contact, setContact] = useState({
+    name: "",
+    lastname: "",
+    email: "",
+    phone: "",
+  });
+  // ============ change Function ============
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+
+  // ============ submit Function ============
+  const submitHandler = () => {
+    console.log(contact);
+  };
+
+  // ============ Rendering ============
+  return (
+    <div className="mt-10 flex items-center justify-center">
+      <div className="border p-8 rounded-lg shadow-lg max-w-lg w-full">
+        <h2 className="text-2xl font-bold text-gray-700 mb-6 text-center">
+          Add a New Contact
+        </h2>
+        <form className="grid grid-cols-2 gap-4">
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={contact.name}
+            onChange={changeHandler}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100"
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Last Name"
+            value={contact.lastname}
+            onChange={changeHandler}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100"
+          />
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={contact.email}
+            onChange={changeHandler}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100"
+          />
+          <input
+            name="phone"
+            type="number"
+            placeholder="Phone"
+            value={contact.phone}
+            onChange={changeHandler}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100"
+          />
+          <button
+            type="button"
+            onClick={submitHandler}
+            className="col-span-2 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-100 hover:text-blue-500 transition"
+          >
+            Add Contact
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
